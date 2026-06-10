030fc5c9df1fc635441767b56d2192ceaee85928
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2026-06-10 22:11:32 +0000

    feat: remove Facebook authentication provider from OAuth options

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ import {
     getAuth,
     GoogleAuthProvider,
     OAuthProvider,
-    FacebookAuthProvider,
     GithubAuthProvider,
     TwitterAuthProvider,
     signInWithPopup,
@@ -738,11 +737,6 @@ const OAUTH_PROVIDERS = {
         label: "Microsoft",
         factory: () => new OAuthProvider("microsoft.com"),
         icon: `<svg class="provider-icon" viewBox="0 0 23 23" aria-hidden="true"><path fill="#f25022" d="M1 1h10v10H1z"/><path fill="#7fba00" d="M12 1h10v10H12z"/><path fill="#00a4ef" d="M1 12h10v10H1z"/><path fill="#ffb900" d="M12 12h10v10H12z"/></svg>`
-    },
-    facebook: {
-        label: "Facebook",
-        factory: () => new FacebookAuthProvider(),
-        icon: `<svg class="provider-icon" viewBox="0 0 24 24" aria-hidden="true"><path fill="#1877F2" d="M24 12.07C24 5.41 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.79-4.69 4.53-4.69 1.31 0 2.68.24 2.68.24v2.97h-1.5c-1.49 0-1.96.93-1.96 1.89v2.25h3.32l-.53 3.49h-2.8V24C19.62 23.1 24 18.1 24 12.07z"/></svg>`
     },
     github: {
         label: "GitHub",
@@ -1539,7 +1533,6 @@ function getReauthProvider() {
             provider.setCustomParameters({ prompt: "select_account" });
             return provider;
         }
-        case "facebook.com": return new FacebookAuthProvider();
         case "github.com": return new GithubAuthProvider();
         case "twitter.com": return new TwitterAuthProvider();
         case "microsoft.com": return new OAuthProvider("microsoft.com");
